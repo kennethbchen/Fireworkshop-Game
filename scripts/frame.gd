5447ef8e542988badf0ffecde9528a2b2c1e1467
kennethbchen <kennethbchen@gmail.com>
2024-10-11 17:16:37 -0400

    Made helper methods for frame

--- scripts/frame.gd
+++ scripts/frame.gd
@@ -21,5 +21,38 @@ func _ready():
 	print(texture.get_image().get_pixel(0,0))
 	size = Vector2(width, height)
 
+
+# NOTE: 0 Indexed
+func get_pix_from_global_position(pos: Vector2) -> Vector2:
+	
+	if not is_global_position_in_bounds(pos):
+		# Out of bounds
+		return Vector2(-1, -1)
+		
+	var relative_position: Vector2 = pos - global_position
+	
+	# Account for scale of frame
+	relative_position *= scale
+	relative_position = relative_position.floor()
+
+	return relative_position
+		
+
+		
+# NOTE: Probably doesn't work if frame is rotated
+func is_global_position_in_bounds(pos: Vector2) -> bool:
+	return get_rect().has_point(pos)
+	
+func is_pix_in_bounds(pos: Vector2) -> bool:
+	
+	if pos.x < 0 or pos.y < 0 or \
+	   pos.x >= width or pos.y >= height:
+		
+		return false
+	
+	return true
+
+
+
 func _process(delta):
 	pass
