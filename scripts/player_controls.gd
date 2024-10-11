379d2105817e2d1cbfe7bbda6a1b616a510423aa
kennethbchen <kennethbchen@gmail.com>
2024-10-11 17:48:29 -0400

    Moved drawing to process function

--- scripts/player_controls.gd
+++ scripts/player_controls.gd
@@ -8,18 +8,17 @@ func _ready():
 	pass # Replace with function body.
 
 func _input(event):
-	print(event)
 	if event is InputEventMouseButton and event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
 		if event.is_pressed():
 			tool_active = true
 			
 		if event.is_released():
 			tool_active = false
+
+
+func _process(delta):
 	
-	if event is InputEventMouseMotion:
-		
-		if tool_active:
-			frame.set_pixel_from_global_position(get_viewport().get_mouse_position(), Color.BLUE)
-			print(frame.is_global_position_in_bounds(get_viewport().get_mouse_position()))
-			
+	if tool_active:
+		frame.set_pixel_from_global_position(get_viewport().get_mouse_position(), Color.BLUE)
+
 
