46de80575630ca1bb0c5142de3903a61194007ac
kennethbchen <kennethbchen@gmail.com>
2024-10-12 13:36:09 -0400

    Made pencil and eraser work on click

--- scripts/tools/pencil_tool.gd
+++ scripts/tools/pencil_tool.gd
@@ -1,5 +1,9 @@
 extends Tool
 
+func on_mouse_pressed(mouse_position: Vector2, frame: Frame):
+	super(mouse_position, frame)
+	frame.set_pixel_from_global_position(mouse_position, Color.BLUE)
+
 func on_mouse_motion(mouse_position: Vector2, frame: Frame):
 	if active:
 		frame.set_pixel_from_global_position(mouse_position, Color.BLUE)
