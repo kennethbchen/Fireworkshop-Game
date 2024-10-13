a224cb13e3d9c1d02919f97642dee681a64af786
kennethbchen <kennethbchen@gmail.com>
2024-10-12 20:24:24 -0400

    Made cursor to indicate selected color

--- scripts/swatch_picker.gd
+++ scripts/swatch_picker.gd
@@ -2,11 +2,11 @@ extends Control
 
 @export var colors: Array[Color]
 
-@onready var grid_container: GridContainer = $SwatchContainer
+@onready var cursor: Control = $SelectionCursor
+
+@onready var swatch_container: GridContainer = $SwatchContainer
 
 var swatch_size: int = 32
-
-var rows: int = 1
 
 var selected_color: Color
 
@@ -14,13 +14,13 @@ func _ready():
 	
 	selected_color = colors[0]
 	
-	grid_container.size.x = swatch_size
-	grid_container.size.y = swatch_size * colors.size()
+	swatch_container.size.x = swatch_size
+	swatch_container.size.y = swatch_size * colors.size()
 	
 	for i in range(colors.size()):
 		
 		var col_rect: ColorRect = ColorRect.new()
-		grid_container.add_child(col_rect)
+		swatch_container.add_child(col_rect)
 		col_rect.color = colors[i]
 		col_rect.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 		col_rect.size_flags_vertical = Control.SIZE_EXPAND_FILL
@@ -29,9 +29,10 @@ func _ready():
 func _on_swatch_gui_input(event: InputEvent, ind: int):
 	
 	if event is InputEventMouseButton and event.is_pressed():
-		print(colors[ind])
 		selected_color = colors[ind]
+		
+		var tween = get_tree().create_tween()
+		tween.set_ease(Tween.EASE_OUT)
+		tween.set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property(cursor, "position", Vector2(0, swatch_size * ind), 0.25)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
