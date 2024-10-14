408e5872f34a991bc37d93aa157543d04db0fbe7
kennethbchen <kennethbchen@gmail.com>
2024-10-13 20:49:46 -0400

    Moved flipbook navigator to control

--- scripts/flipbook_navigator.gd
+++ scripts/flipbook_navigator.gd
@@ -1,8 +1,10 @@
-extends HBoxContainer
+extends Control
 
+@onready var frame_parent = $ScrollContainer/VBoxContainer/HBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(frame_parent)
 	pass # Replace with function body.
 
 
@@ -11,28 +13,27 @@ func _process(delta):
 	pass
 	
 func _on_current_flipbook_changed(new_flipbook: SpriteFrames):
-	
 	# Make sure that number of texture_rect children match the new flipbook's frame count
-	if get_child_count() > new_flipbook.get_frame_count("default"):
+	if frame_parent.get_child_count() > new_flipbook.get_frame_count("default"):
 		# Remove excess children
-		for i in range(get_child_count() - new_flipbook.get_frame_count("default")):
-			var child = get_children().pop_back()
+		for i in range(frame_parent.get_child_count() - new_flipbook.get_frame_count("default")):
+			var child = frame_parent.get_children().pop_back()
 			child.queue_free()
 	
-	if get_child_count() < new_flipbook.get_frame_count("default"):
+	if frame_parent.get_child_count() < new_flipbook.get_frame_count("default"):
 		# Add more children
 		
-		var init_children_count: int = get_child_count()
-		for i in range(new_flipbook.get_frame_count("default") - get_child_count()):
+		var init_children_count: int = frame_parent.get_child_count()
+		for i in range(new_flipbook.get_frame_count("default") - frame_parent.get_child_count()):
 			var child: TextureRect = TextureRect.new()
-			add_child(child)
+			frame_parent.add_child(child)
 			
 			child.gui_input.connect(func(event): _on_frame_gui_input(event, init_children_count + i))
 			
 	
 	# Display new frames
 	for i in range(new_flipbook.get_frame_count("default")):
-		get_children()[i].texture = new_flipbook.get_frame_texture("default", i)
+		frame_parent.get_children()[i].texture = new_flipbook.get_frame_texture("default", i)
 
 func _on_frame_gui_input(event: InputEvent, idx: int):
 	
